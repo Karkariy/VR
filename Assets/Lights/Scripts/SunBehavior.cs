f43b74940f9c06dbf5dd06b74b60e7a282f258e4
Karkariy <helali44@hotmail.fr>
2016-11-16 22:56:41 +0100

    clock progress

--- Assets/Lights/Scripts/SunBehavior.cs
+++ Assets/Lights/Scripts/SunBehavior.cs
@@ -1,33 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class SunBehavior : MonoBehaviour {
 
     // levée du soleil à Dehzou ~ 7h
     //0 deg rot X -> 8h
 
     public Light m_SunLight;
-    public int m_hour;
-    public int m_minutes;
+    public Clock m_clock;
+    
+  
 
 	// Use this for initialization
 	void Start () {
-        Vector3 sunRotation = transformTimeToRotation(m_hour, m_minutes);
-        m_SunLight.transform.localEulerAngles= sunRotation;
+        
     }
 
     // Update is called once per frame
     void Update () {
-	
 
+        m_SunLight.transform.Rotate(Vector3.right * Time.deltaTime* transformTimeToRotation(m_clock.GetHours, m_clock.GetMinutes), Space.World); 
 
-	}
+    }
 
-    private Vector3 transformTimeToRotation(int m_hour, int m_minutes)
+    private float transformTimeToRotation(int hour, int minutes)
     {
-        int x = 0;
-        Vector3 sunRotation = new Vector3(x,180,0);
-        return sunRotation;
+        Debug.Log("position : " + transform.rotation.eulerAngles);
+
+        if (hour > 6 && hour < 17)
+            return transform.rotation.eulerAngles.x + 1.0f;
+        else if (hour == 6)
+            return 0.0f;
+        else if (hour == 17)
+            return 0.0f;
+
+        return 0.0f;
     }
 
 }
