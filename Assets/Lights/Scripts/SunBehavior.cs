7422a7666cfeb03852d92037db643b876a10e164
Karkariy <helali44@hotmail.fr>
2016-11-19 19:53:17 +0100

    Light update

--- Assets/Lights/Scripts/SunBehavior.cs
+++ Assets/Lights/Scripts/SunBehavior.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 
 public class SunBehavior : MonoBehaviour {
@@ -7,43 +7,57 @@ public class SunBehavior : MonoBehaviour {
     // levée du soleil à Dehzou ~ 7h
     //0 deg rot X -> 8h
 
-    public Light m_SunLight;
-    public Clock m_clock;
-    
-  
+    public Light m_sunLight;
+    private DateTime m_clock;
+    public int m_sunrise;
+    public int m_sunset;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+        m_clock = DateTime.Now;
+
+        float x = ((m_clock.Hour + m_clock.Minute/60.0f) - m_sunrise)/((m_sunset - m_sunrise))*180.0f;
+
+        m_sunLight.transform.eulerAngles = new Vector3(x, 180.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update () {
+        //m_clock = m_clock.AddSeconds(Time.deltaTime);
+        m_clock = m_clock.AddMinutes(Time.deltaTime*100.0f);
 
-        m_SunLight.transform.Rotate(Vector3.right * Time.deltaTime* transformTimeToRotation(m_clock.GetHours, m_clock.GetMinutes), Space.World); 
+        float x = ((m_clock.Hour + m_clock.Minute / 60.0f) - m_sunrise) / ((m_sunset - m_sunrise)) * 180.0f;
+        transform.eulerAngles = new Vector3(x, 180.0f,0.0f);
+        updateIntensity(m_clock);
 
     }
 
-    private float transformTimeToRotation(int hour, int minutes)
+    private void updateIntensity(DateTime time)
     {
-        Debug.Log("position : " + transform.rotation.eulerAngles);
+        float intensity = -1.0f;
+        if (time.Hour <= (m_sunrise + m_sunset) / 2 && time.Hour >= m_sunrise)
+        {
+            intensity = Mathf.Clamp(time.Hour, m_sunrise, ((m_sunrise + m_sunset) / 2));
+            Debug.LogWarning(intensity);
+        }
+        else if(time.Hour < m_sunrise || time.Hour> m_sunset)
+        {
+            intensity = 5.0f;
+            Debug.Log("Ok");
 
-        if (hour > 6 && hour < 17)
-        {
-            m_SunLight.intensity = 0.5f;
-            return transform.rotation.eulerAngles.x + 0.002f;
         }
-        else if (hour == 6)
+        else
         {
-            m_SunLight.intensity = 0.1f;
-            return 0.0f;
-        }
-        else if (hour == 17)
-        {
-            m_SunLight.intensity = 0.1f;
-            return 0.0f;
+            float max = 1.0f / ((m_sunrise + m_sunset) / 2.0f);
+            float min = 1.0f / m_sunset;
+
+            intensity = Mathf.Clamp(time.Hour, min * 100, max * 100);
+            Debug.Log(intensity);
         }
 
-        return 0.0f;
+        m_sunLight.intensity = 0.05f * intensity;
+
     }
 
 }
